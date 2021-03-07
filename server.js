f15f5687662824d7cd8b1e2c74063ac0e24368bd
Greyson Mainor <gbmainor98@outlook.com>
2021-03-06 19:05:44 -0500

    Attempting to set up my Express server.

--- server.js
+++ server.js
@@ -3,3 +3,12 @@ const fs = require("fs");
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static(__dirname));
+
+app.listen(PORT, function() {
+    console.log("Listening on PORT:" + PORT)
+});
