e5de01319d5824db30cd036568ba0874e224c06e
Greyson Mainor <gbmainor98@outlook.com>
2021-03-06 20:58:41 -0500

    atempting to get my app to work once its deployed.

--- routes/route.js
+++ routes/route.js
@@ -5,7 +5,7 @@ module.exports = app => {
     fs.readFile("db/db.json","utf8", (err, data) =>{
         if (err) throw err;
 
-        let note = JSON.parse(data);
+        var note = JSON.parse(data);
 
         app.get("/notes", function(req, res){
             res.sendFile(path.join(__dirname, "../public/notes.html"));
@@ -14,6 +14,25 @@ module.exports = app => {
             res.sendFile(path.join(__dirname, "../public/index.html"));
         });
 
+        function dataBaseRefresh() {
+            fs.writeFile("db/db.json", JSON.stringify(notes,"\t"),err => {
+                if (err) throw err;
+                return true;
+            });
+        };
+        app.get("/api/notes", function(req, res){
+            res.json(note);
+        });
+        app.post("/api/notes", function(req,res){
+            let nextNote = req.body;
+            note.push(nextNote);
+            dataBaseRefresh();
+            return console.log("added new note!")
+        });
+        app.get("/api/notes/:id", function(req,res){
+            res.json(notes[req.params.id]);
+        });
+        
     })
 }
 
--- server.js
+++ server.js
@@ -4,13 +4,11 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+require("./routes/route")(app);
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static(__dirname));
-
-require("./routes/route")(app);
-
 app.listen(PORT, function() {
     console.log("Listening on PORT:" + PORT)
 });
